39193c40ec6a45cfbcfd6872665ed1620b6519bc
Spartfin <fin.07@bk.ru>
2020-10-05 18:25:44 +0300

    Правки по стайлу

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,53 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div>
+        <nuxt />
+    </div>
 </template>
 
 <style lang="scss">
 html {
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+    box-sizing: border-box;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
 }
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    margin: 0;
 }
 
 .button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+    display: inline-block;
+    padding: 10px 30px;
+    text-decoration: none;
+    color: #3b8070;
+    border: 1px solid #3b8070;
+    border-radius: 4px;
 }
 
 .button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+    color: #fff;
+    background-color: #3b8070;
 }
 
 .button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+    display: inline-block;
+    padding: 10px 30px;
+    text-decoration: none;
+    color: #35495e;
+    border: 1px solid #35495e;
+    border-radius: 4px;
+    margin-left: 15px;
 }
 
 .button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+    color: #fff;
+    background-color: #35495e;
 }
 </style>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,60 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Banners
-      </h1>
-      <h2 class="subtitle">
-        Здесь должно быть описание))
-      </h2>
-      <div class="links">
-        <a
-          href="/banners-list"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+    <section class="container">
+        <div>
+            <app-logo />
+            <h1 class="title">Banners</h1>
+            <h2 class="subtitle">Здесь должно быть описание)) </h2>
+            <div class="links">
+                <a
+                    href="/banners-list"
+                    class="button--green"
+                >Documentation</a>
+                <a
+                    href="https://github.com/nuxt/nuxt.js"
+                    target="_blank"
+                    class="button--grey"
+                >GitHub</a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from '../components/AppLogo.vue';
 
 export default {
-  components: {
-    AppLogo
-  }
-}
+    components: {
+        AppLogo,
+    },
+};
 </script>
 
 <style lang="scss">
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    text-align: center;
 }
 
 .title {
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    display: block;
+    font-size: 100px;
+    font-weight: 300;
+    letter-spacing: 1px;
+    color: #35495e;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+    padding-bottom: 15px;
+    font-size: 42px;
+    font-weight: 300;
+    word-spacing: 5px;
+    color: #526488;
 }
 
 .links {
-  padding-top: 15px;
+    padding-top: 15px;
 }
 </style>
-
